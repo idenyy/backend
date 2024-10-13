a312d15af2300a17388558fdcf86b3b62b283dbd
Denys <d3nys.pavlov@gmail.com>
2024-10-13 18:42:12 +0300

    updated auth/me

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,22 +1,39 @@
 import jwt from "jsonwebtoken";
-import cookie from "cookie";
+
+// export const verifyToken = (req, res, next) => {
+//   const cookies = cookie.parse(
+//     req.headers.cookie || req.headers.authorization?.split(" ")[1],
+//   );
+//
+//   const token = cookies.jwt;
+//
+//   if (!token) {
+//     return res.status(401).json({ message: "Authentication required" });
+//   }
+//
+//   try {
+//     const user = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = user;
+//
+//     next();
+//   } catch (error) {
+//     console.log(`Error in [verifyToken] controller: ${error.message}`);
+//     return res.status(401).json({ message: "Invalid token" });
+//   }
+// };
 
 export const verifyToken = (req, res, next) => {
-  const cookies = cookie.parse(req.headers.cookie || "");
-
-  const token = cookies.jwt;
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Authentication required" });
   }
 
   try {
-    const user = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Розшифровка токена
     req.user = user;
-
     next();
   } catch (error) {
-    console.log(`Error in [verifyToken] controller: ${error.message}`);
     return res.status(401).json({ message: "Invalid token" });
   }
 };
