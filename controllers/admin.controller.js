35f3fb894f50f176cab3f49721d3109ec0f409e7
Denys <d3nys.pavlov@gmail.com>
2024-10-25 21:21:53 +0300

    fix: validate unique email in admin update

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -53,21 +53,30 @@ export const updateAdmin = async (req, res) => {
   const updates = req.body;
 
   try {
-    if (updates.password) {
-      const saltRounds = 10;
-      updates.password = await bcrypt.hash(updates.password, saltRounds);
-    }
-
-    const admin = await Admin.findByIdAndUpdate(id, updates, {
-      new: true,
-      runValidators: true,
-    });
-
+    const admin = await Admin.findById(id);
     if (!admin) {
       return res.status(404).json({ error: "Admin not found" });
     }
 
-    return res.status(200).json(admin);
+    if (updates.email) {
+      if (updates.email === admin.email) {
+        return res
+          .status(400)
+          .json({ error: "New email is the same as the current email" });
+      }
+
+      const emailExists = await Admin.findOne({ email: updates.email });
+      if (emailExists) {
+        return res.status(400).json({ error: "Email is already in use" });
+      }
+    }
+
+    const updatedAdmin = await Admin.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    });
+
+    return res.status(200).json(updatedAdmin);
   } catch (error) {
     console.error(`Error in [updateAdmin] controller: ${error.message}`);
     res.status(500).json({ error: "Internal Server Error" });
