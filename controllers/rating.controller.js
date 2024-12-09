3aacaea3717b6d4f79d875cc6867e10824989385
Denys <d3nys.pavlov@gmail.com>
2024-12-09 19:05:16 +0200

    fix: rating functionality

--- controllers/rating.controller.js
+++ controllers/rating.controller.js
@@ -1,24 +1,32 @@
-import Product from '../models/product.model.js';
-import Rating from '../models/rating.model.js';
+import Product from "../models/product.model.js";
+import Rating from "../models/rating.model.js";
 
 export const addOrUpdateRating = async (req, res) => {
   const { productId, userId, rating } = req.body;
 
   try {
     let userRating = await Rating.findOne({ productId, userId });
-    if (userRating) return res.status(200).json({ message: 'You can only rate once' });
+    if (userRating)
+      return res.status(400).json({ message: "You can only rate once" });
 
     userRating = new Rating({ productId, userId, rating });
     await userRating.save();
 
     const ratings = await Rating.find({ productId });
-    const globalRating = ratings.reduce((sum, item) => sum + item.rating, 0) / ratings.length;
+    const globalRating =
+      ratings.reduce((sum, item) => sum + item.rating, 0) / ratings.length;
 
-    const product = await Product.findByIdAndUpdate(productId, { rating: globalRating }, { new: true });
+    const product = await Product.findByIdAndUpdate(
+      productId,
+      { rating: globalRating },
+      { new: true },
+    );
 
-    res.status(200).json({ message: 'Rating updated', globalRating: product.rating });
+    res
+      .status(200)
+      .json({ message: "Rating updated", globalRating: product.rating });
   } catch (error) {
-    res.status(500).json({ message: 'Error adding rating', error });
+    res.status(500).json({ message: "Error adding rating", error });
   }
 };
 
@@ -28,12 +36,12 @@ export const getProductRating = async (req, res) => {
   try {
     const product = await Product.findById(productId);
     if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
+      return res.status(404).json({ message: "Product not found" });
     }
 
     const ratings = await Rating.find({ productId });
     res.status(200).json({ globalRating: product.rating, ratings });
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching ratings', error });
+    res.status(500).json({ message: "Error fetching ratings", error });
   }
 };
